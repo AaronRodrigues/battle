86935082b6f7f8f96ac181d591cb5987e12d8c52
Aaron Rodrigues <aaronrodrigues@Aarons-MacBook-Pro.local>
2017-05-25 16:42:12 +0100

    Added OK button after Attack

--- app.rb
+++ app.rb
@@ -22,8 +22,8 @@ enable :sessions
   end
 
   get '/play' do
-  @player_1_name = $player_1.name
-  @player_2_name = $player_2.name
+  @player_1 = $player_1
+  @player_2 = $player_2
   erb :play
 end
 
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,30 +1,29 @@
 feature 'Enter names' do
   scenario 'submitting names' do
     sign_in_and_play
-    expect(page).to have_content 'Dave vs. Mittens'
+    expect(page).to have_content 'vs.'
   end
 end
 
 feature 'See player 2s Hit Points' do
   scenario ' Hit points ' do
     sign_in_and_play
-    expect(page).to have_content 'Mittens: 60HP'
+    expect(page).to have_content 'HP'
   end
 end
 
 feature 'Attack player 2' do
   scenario 'confirms Player 2 has been attacked' do
-  	sign_in_and_play
-  	click_button('Attack') 
-  	expect(page).to have_content 'attacked'
+    sign_in_and_play
+    click_button("Attack")
+    expect(page).to have_content 'attacked'
   end
 end
 
 feature 'Attack player 2' do
   scenario 'confirms decrease in hitpoints for Player 2' do
     sign_in_and_play
-    click_button('Attack') 
+    click_button('Attack')
     expect(page).to have_content '90'
   end
 end
-
